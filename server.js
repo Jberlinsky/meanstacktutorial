3b0ec09146dedb3dd06a8ba0bd3b83096f7308e7
Jason Berlinsky <jberlinsky@gmail.com>
2018-09-12 20:42:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express'); // ExperssJS Framework
 var app = express(); // Invoke express to variable for use in application
 var port = process.env.PORT || 8080; // Set default port or assign a port in enviornment
-var morgan = require('morgan'); // Import Morgan Package
 var mongoose = require('mongoose'); // HTTP request logger middleware for Node.js
 var bodyParser = require('body-parser'); // Node.js body parsing middleware. Parses incoming request bodies in a middleware before your handlers, available under req.body.
 var router = express.Router(); // Invoke the Express Router
@@ -10,7 +9,6 @@ var path = require('path'); // Import path module
 var passport = require('passport'); // Express-compatible authentication middleware for Node.js.
 var social = require('./app/passport/passport')(app, passport); // Import passport.js End Points/API
 
-app.use(morgan('dev')); // Morgan Middleware
 app.use(bodyParser.json()); // Body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(express.static(__dirname + '/public')); // Allow front end to access public folder
